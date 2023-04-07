79d80104647b8b21c98a199a76f266e3b1e6d4f7
MariamMek <124275844+MariamMek@users.noreply.github.com>
2023-04-07 18:21:14 +0400

    log inputs

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'mariams blog';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
+console.log(inputs);
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'mariams blog'
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    console.log(input); 
+})
+
+console.log(inputs);
